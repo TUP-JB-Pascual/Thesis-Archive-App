e9a22941eff8fbb87b71271b2f51af653aea2162
John Brix Pascual <johnbrixpascual10@gmail.com>
2024-09-22 06:17:05 +0800

    pdf error

--- ThesisArchive/settings.py
+++ ThesisArchive/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #apps
     'website',
 ]
 
@@ -37,6 +39,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTH_USER_MODEL = 'website.CustomUser'
 
 ROOT_URLCONF = 'ThesisArchive.urls'
 
@@ -124,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media'
--- website/views.py
+++ website/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import RegisterUserForm, ThesisForm
+from django.http import FileResponse
 
-# Create your views here.
+def home(request):
+    context = {}
+    # Check to see if logging in
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful")
+            return redirect('home')
+        else:
+            messages.success(request, "Incorrect Credentials. Try Again.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', context)
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have Logged Out.")
+    return redirect('home')
+
+def register_user(request):
+    context = {}
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(email=email, password=password)
+            login(request, user)
+            messages.success(request, "You have Succesfully Registered.")
+            return redirect('home')
+        else:
+            context['form'] = form
+    else:
+        form = RegisterUserForm()
+        context['form'] = form
+    return render(request, 'register.html', context)
+
+def publish(request):
+    context = {}
+    if request.method == 'POST':
+        form = ThesisForm(request.POST, request.FILES)
+        if form.is_valid():
+            '''
+            published_date = form.cleaned_data.get('published_date')
+            title = form.cleaned_data.get('title')
+            author = form.cleaned_data.get('author')
+            pdf_file = form.cleaned_data.get('pdf_file')
+            '''
+            form.save()
+        else:
+            context['form'] = form
+    else:
+        form = ThesisForm()
+        context['form'] = form
+    return render(request, 'publish.html', context)
